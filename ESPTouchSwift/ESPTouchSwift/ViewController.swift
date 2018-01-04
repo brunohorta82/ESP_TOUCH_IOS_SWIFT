394568fed374505ef0d01af4a0988be86327b385
Bruno Horta <bh@void.pt>
2018-01-04 18:04:24 +0000

    no message

--- ESPTouchSwift/ESPTouchSwift/ViewController.swift
+++ ESPTouchSwift/ESPTouchSwift/ViewController.swift
@@ -14,19 +14,26 @@ class ViewController: UIViewController, ESPTouchDelegate {
     var strLabel = UILabel()
     let effectView = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
     var results : Array<ESPTouchResult> = Array()
-    var resultExpected = 1
+    var resultExpected = 0
     var alertController = UIAlertController()
     var esptouchTask : ESPTouchTask?
     var messageResult = ""
     var resultCount = 0
+    var okAction:UIAlertAction?
     func onEsptouchResultAdded(with result: ESPTouchResult!) {
       results.append(result)
         DispatchQueue.main.async {
+            if(self.results.count == self.resultExpected ){
+                self.esptouchTask?.interrupt()
+                if let ok = self.okAction{
+                    ok.isEnabled = true
+                }
+                
+            }
             self.effectView.removeFromSuperview()
             self.messageResult  = "\(self.messageResult)\(result.bssid!) - ip: \(ESP_NetUtil.descriptionInetAddr4(by: result.ipAddrData)!)\n"
             self.resultCount = self.resultCount + 1
             self.alertController.title = "\(self.resultCount) ESP\(self.resultCount > 1 ? "(s)" :" ") encontrado\(self.resultCount > 1 ? "s" :" ")"
-            
             self.alertController.message = self.messageResult
         }
     }
@@ -46,7 +53,7 @@ class ViewController: UIViewController, ESPTouchDelegate {
             self.esptouchTask = ESPTouchTask.init(apSsid: self.ssidLabel.text, andApBssid: self.bssid, andApPwd: self.passwordInputText.text)
             if let task = self.esptouchTask{
                 task.setEsptouchDelegate(self)
-                let  results: NSArray = (task.execute(forResults: Int32(self.resultExpected))! as NSArray)
+                let  results: NSArray = (task.execute(forResults: Int32(self.resultExpected == 0 ?  Int.max : self.resultExpected))! as NSArray)
                 print(results)
             }
             
@@ -54,7 +61,7 @@ class ViewController: UIViewController, ESPTouchDelegate {
     }
     @IBAction func onNumberDevicesChange(_ sender: UISlider) {
         resultExpected = Int(sender.value)
-        numberOfDevicesLabel.text = resultExpected.description
+        numberOfDevicesLabel.text = resultExpected == 0 ? "Todos" : resultExpected.description
        
     }
     
@@ -69,8 +76,13 @@ class ViewController: UIViewController, ESPTouchDelegate {
         
          alertController = UIAlertController(title: title, message:
             message, preferredStyle: UIAlertControllerStyle.alert)
-        alertController.addAction(UIAlertAction(title: "Cancelar", style: UIAlertActionStyle.default,handler: nil))
         
+        alertController.addAction(UIAlertAction(title: "Cancelar", style: UIAlertActionStyle.cancel,handler: { action in self.esptouchTask?.interrupt()}))
+        self.okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default,handler: nil)
+        if let ok = self.okAction {
+            ok.isEnabled = false
+            alertController.addAction(ok)
+        }
         self.present(alertController, animated: true, completion: nil)
     
     }
